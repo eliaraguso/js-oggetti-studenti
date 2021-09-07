9677298078e86ac8ec50da1bc41e7b5f30f16928
eliaraguso <elia.raguso@gmail.com>
2021-09-07 15:10:22 +0200

    creato ciclo for in per mostrare a schermo tutte le proprietà dell oggetto studente

---
+++ main.js
@@ -0,0 +1,27 @@
+/*Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.*/
+
+
+// Creo un oggetto studente con le proprietà: nome, cognome, età.
+
+let studente = {
+    "nome" : "Darth",
+    "cognome" : "Vader",
+    "eta" : 30
+};
+
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for (let key in studente) {
+        
+    document.getElementById("objectProperties").innerHTML += studente[key] ;
+
+}
+    
+
+
+    
+
+
